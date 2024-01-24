da53faf9079d469b5669f861496f77510208d9b0
DanielC218 <danielcarrer2007@gmail.com>
2024-01-23 20:26:07 -0400

    Diagrama de Flujo

--- AñoBisiestoDC.py
+++ AñoBisiestoDC.py
@@ -12,3 +12,5 @@ elif año % 4 == 0 and año % 100 == 0 and año % 400 == 0:
     bisiesto = bisiesto + 1
     
 print("Enetre 1900 y " + str(año_usuario) + " hay " + str(bisiesto) + " año(s) biciestro(s)")
+
+#Profe le deje el link al diagrama de flujo en la tarea por classroom
