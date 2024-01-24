4c6e817908ab0b7509152b29a14ecb4de1c12dd7
DanielC218 <danielcarrer2007@gmail.com>
2024-01-24 01:26:40 -0400

    Batalla Pokemon V4

--- BatallaPokemonDC.py
+++ BatallaPokemonDC.py
@@ -1,34 +1,40 @@
+#Variables básicas e imports del programa
+
 import random
 
 username = str((input("Escribe tu nombre para empezar: "))).lower()
-HP_jugador = 175 #HP es health points = puntos de salud
-HP_Ash = 175
-PD_jugador = 105 #PD es player defense = defensa del jugador
-Ash_D = 105
+HP_jugador = 200 #HP es health points = puntos de salud
+HP_Ash = 200
+PD_jugador = 150 #PD es player defense = defensa del jugador
+Ash_D = 150
 Atq_jugador = ["malicioso","placaje", "ascuas"]
 Atq_oponente = ["latigo","placaje", "pistola agua"]
 
+# Introducción 
+
 print()
 print("--------------------------------------------------")
-print("  BIENVENIDOS A LA FINAL DE ENTRENADORES POKEMON  ") #bienvenida
+print("  BIENVENIDOS A LA FINAL DE ENTRENADORES POKEMON  ") 
 print("--------------------------------------------------")
 print()
 print(input("(presiona enter para continuar)"))
 print()
-print(input ("¡¡Hoy los entrenadores más fuertes de la región Kanto se enfrentaran por obtener la última medalla de gimnasio!! ")) #inicio introducción
+print(input ("¡¡Hoy los entrenadores más fuertes de la región Kanto se enfrentaran por obtener la última medalla de gimnasio!! ")) 
 print(input ("Nuestros competidores son Ash y " + username))
 print(input("¿Podrá " + username + " romper la racha de victorias de Ash?"))
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~          ")
 print("  empiezan a aparecer dudas en la cabeza de " + username + ", sin embargo, dará todo en la pelea  ")
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~          ")
 print()
-print("Lo veremos en un momento, ¡Que comience la batalla!") #final introducción
+print("Lo veremos en un momento, ¡Que comience la batalla!") 
 print()
+
+#Desarrollo - Bucle
 
 while HP_jugador > 0 and HP_Ash > 0: #ciclo de la pelea
     print("-------------------------------------------------------------------------------")
     print()
-    print("                                 ELIJE TU ATAQUE:                              ")
+    print("                          ¡ES TU TURNO, ELIJE TU ATAQUE!                       ")
     print()
     print("     [ ================= ]     [ ================ ]     [ ============== ]     ") 
     print("     | ¿¡¿ Malicioso ?!? |     |  ¿¡¿ Plajage ?!? |     | ¿¡¿ Ascuas ?!? |     ")  #menu de ataques del jugador
@@ -39,9 +45,9 @@ while HP_jugador > 0 and HP_Ash > 0: #ciclo de la pelea
     print()
 
     Atq_elegido = str(input(" Usaré... ").lower())
-    Atq_esquivado = random.randrange(1,150)               #probabilidad de que el oponente esquive el ataque
-    Atq_critico = random.randrange (1,200)          #probabilidad de que el jugador haga un ataque crítico
-    Atq_normal = random. randrange (1,250)          #probabilidad de que el jugador haga un ataque normal
+    Atq_esquivado = random.randrange(1,150)             #probabilidad de que el oponente esquive el ataque
+    Atq_critico = random.randrange (1,200)              #probabilidad de que el jugador haga un ataque crítico
+    Atq_normal = random. randrange (1,250)              #probabilidad de que el jugador haga un ataque normal
 
     if Atq_critico > Atq_normal and Atq_critico > Atq_esquivado: 
         print("==========================                       ")
@@ -49,14 +55,12 @@ while HP_jugador > 0 and HP_Ash > 0: #ciclo de la pelea
         print("==========================                       ")
         print()
         Critico = 1.5
-
     elif Atq_esquivado > Atq_normal and Atq_esquivado > Atq_critico:
         print("==========================                       ")
         print("¡¡" + username + " ha usado " + Atq_elegido + "!!") #feedback al usuario
         print("==========================                       ")
         print()
         Esquivo = 0.5
-    
     else: 
         print("==========================                       ")
         print("¡¡" + username + " ha usado " + Atq_elegido + "!!") #feedback al usuario
@@ -65,76 +69,199 @@ while HP_jugador > 0 and HP_Ash > 0: #ciclo de la pelea
 
     #Turno del jugador
 
-    if Atq_elegido == Atq_jugador[0]: #condición empleando como base el primer término de la lista atq_jugador
-        Ash_D = Ash_D - random.randrange(10,12)
-        if Atq_critico > Atq_normal and Atq_critico > Atq_esquivado:
+    if Atq_elegido == Atq_jugador[0]: #condición para ejecutar ataque Malicioso
+        Ash_D = Ash_D - random.randrange(10,13)
+        if Atq_critico > Atq_normal and Atq_critico > Atq_esquivado: #Condición para provocar un ataque crítico 
+            Ash_D = Ash_D * Critico
+            Ash_D = str(Ash_D)
+            print("=========================================================            ") 
+            print("¡¡ Haz hecho un CRíTICO y Ash tiene ahora " + Ash_D + " de defensa !!") #feedback al usuario
+            print("=========================================================            ")
+            print()
+        elif Atq_esquivado > Atq_normal and Atq_esquivado > Atq_critico: #Condición para que Ash esquive el ataque
+            Ash_D = Ash_D * Esquivo
+            Ash_D = str(Ash_D)
+            print("=================================================             ") 
+            print("¡¡ Ash a ESQUIVADO y tiene ahora " + Ash_D + " de defensa !!  ") #feedback al usuario
+            print("=================================================             ")
+            print()
+        else: #Condiciones sobrantes representan el ataque normal
+            Ash_D = str(Ash_D)
+            print("==============================================               ") 
+            print("¡¡ Excelente, Ash tiene ahora " + Ash_D + " de defensa !!    ") #feedback al usuario
+            print("==============================================               ")
+            print()
+        if Ash_D <= 0:
+            Ash_D = 1 
+    elif Atq_elegido == Atq_jugador[1]: #condición para ejecutar ataque Placaje 
+        HP_Ash = HP_Ash - 35 * round(100/Ash_D)
+        if Atq_critico > Atq_normal and Atq_critico > Atq_esquivado: #Condición para provocar un ataque crítico 
+            HP_Ash = HP_Ash * Critico
+            HP_Ash = str(HP_Ash)
+            print("=======================================================            ") 
+            print("¡¡ Haz hecho un CRíTICO y Ash tiene ahora " + HP_Ash + " de vida !!") #feedback al usuario
+            print("=======================================================            ")
+            print()
+        elif Atq_esquivado > Atq_normal and Atq_esquivado > Atq_critico: #Condición para que Ash esquive el ataque
+            HP_Ash = HP_Ash * Esquivo
+            HP_Ash = str(HP_Ash)
+            print("==============================================             ") 
+            print("¡¡ Ash a ESQUIVADO y tiene ahora " + HP_Ash + " de vida !!    ") #feedback al usuario
+            print("==============================================             ")
+            print()
+        else: #Condiciones sobrantes representan el ataque normal
+            HP_Ash = str(HP_Ash)
+            print("=======================================            ") 
+            print("¡¡Muy bien, Ash tiene ahora " + HP_Ash + "de vida!!") #feedback al usuario
+            print("=======================================            ")
+            print()
+    elif Atq_elegido == Atq_jugador[2]: #condición para ejecutar ataque Ascuas
+        HP_Ash = HP_Ash - random.randrange(22,49)
+        if Atq_critico > Atq_normal and Atq_critico > Atq_esquivado: #Condición para provocar un ataque crítico 
+            HP_Ash = HP_Ash * Critico
+            HP_Ash = str(HP_Ash)
+            print("=======================================================            ") 
+            print("¡¡ Haz hecho un CRíTICO y Ash tiene ahora " + HP_Ash + " de vida !!") #feedback al usuario
+            print("=======================================================            ")
+            print()
+        elif Atq_esquivado > Atq_normal and Atq_esquivado > Atq_critico: #Condición para que Ash esquive el ataque
+            HP_Ash = HP_Ash * Esquivo
+            HP_Ash = str(HP_Ash)
+            print("==============================================             ") 
+            print("¡¡ Ash a ESQUIVADO y tiene ahora " + HP_Ash + " de vida !!    ") #feedback al usuario
+            print("==============================================             ")
+            print()
+        else: #Condiciones sobrantes representan el ataque normal        
+            HP_Ash = str(HP_Ash)
+            print("========================================            ") 
+            print("¡¡Sigue así, Ash tiene ahora " + HP_Ash + "de vida!!") #feedback al usuario
+            print("========================================            ")
+            print()
+
+    print("------------------------------------------------              ")
+    print("             ES EL FIN DE TU TURNO                            ")
+    print("                                                              ")
+    print("   VIDA DE ASH = " + HP_Ash + "     DEFENSA DE ASH = " + Ash_D )
+    print("------------------------------------------------              ")
+    Ash_D = int(Ash_D)
+    HP_Ash = int(HP_Ash)
+
+    #Turno de Ash
+
+    Atq_Ash = random.randrange(0,3)
+    print("----------------------")
+    print("  ES EL TURNO DE ASH  ")
+    print("----------------------")
+    
+    Atq_esquivado = random.randrange(1,165)             #probabilidad de que el oponente esquive el ataque
+    Atq_critico = random.randrange (1,215)              #probabilidad de que el jugador haga un ataque crítico
+    Atq_normal = random. randrange (1,265)              #probabilidad de que el jugador haga un ataque normal
+    
+    if Atq_Ash == Atq_oponente[0]
+        if Atq_critico > Atq_normal and Atq_critico > Atq_esquivado: 
+            print("=========================")
+            print("¡¡ Ash ha usado Látigo !!") #feedback al usuario
+            print("=========================")
+            print()
+            Critico = 1.6
+        elif Atq_esquivado > Atq_normal and Atq_esquivado > Atq_critico:
+            print("=========================")
+            print("¡¡ Ash ha usado Látigo !!") #feedback al usuario
+            print("=========================")
+            print()
+            Esquivo = 0.6
+        else: 
+            print("=========================")
+            print("¡¡ Ash ha usado Látigo !!") #feedback al usuario
+            print("=========================")
+            print()
+    if Atq_Ash == Atq_oponente[1]
+        if Atq_critico > Atq_normal and Atq_critico > Atq_esquivado: 
+            print("=========================")
+            print("¡¡ Ash ha usado Placaje !!") #feedback al usuario
+            print("=========================")
+            print()
+            Critico = 1.6
+        elif Atq_esquivado > Atq_normal and Atq_esquivado > Atq_critico:
+            print("=========================")
+            print("¡¡ Ash ha usado Placaje !!") #feedback al usuario
+            print("=========================")
+            print()
+            Esquivo = 0.6
+        else: 
+            print("=========================")
+            print("¡¡ Ash ha usado Plcaje !!") #feedback al usuario
+            print("=========================")
+            print()
+
+
+
+
+    if Atq_elegido == Atq_jugador[0]: #condición para ejecutar ataque Malicioso
+        Ash_D = Ash_D - random.randrange(10,13)
+        if Atq_critico > Atq_normal and Atq_critico > Atq_esquivado: #Condición para provocar un ataque crítico 
             Ash_D = Ash_D * Critico
             str(Ash_D)
             print("=========================================================            ") 
             print("¡¡ Haz hecho un CRíTICO y Ash tiene ahora " + Ash_D + " de defensa !!") #feedback al usuario
             print("=========================================================            ")
             print()
-        elif Atq_esquivado > Atq_normal and Atq_esquivado > Atq_critico: 
+        elif Atq_esquivado > Atq_normal and Atq_esquivado > Atq_critico: #Condición para que Ash esquive el ataque
             Ash_D = Ash_D * Esquivo
             str(Ash_D)
             print("=================================================             ") 
             print("¡¡ Ash a ESQUIVADO y tiene ahora " + Ash_D + " de defensa !!  ") #feedback al usuario
             print("=================================================             ")
             print()
-        else: 
+        else: #Condiciones sobrantes representan el ataque normal
             str(Ash_D)
             print("==============================================               ") 
-            print("¡¡ Excelente, Ash tiene ahora " + Ash_D + " de defensa !!   ") #feedback al usuario
+            print("¡¡ Excelente, Ash tiene ahora " + Ash_D + " de defensa !!    ") #feedback al usuario
             print("==============================================               ")
             print()
         if Ash_D <= 0:
             Ash_D = 1 
-    elif Atq_elegido == Atq_jugador[1]: #condición empleando como base el segundo término de la lista atq_jugador
+    elif Atq_elegido == Atq_jugador[1]: #condición para ejecutar ataque Placaje 
         HP_Ash = HP_Ash - 35 * round(100/Ash_D)
-        if Atq_critico > Atq_normal and Atq_critico > Atq_esquivado:
+        if Atq_critico > Atq_normal and Atq_critico > Atq_esquivado: #Condición para provocar un ataque crítico 
             HP_Ash = HP_Ash * Critico
             str(HP_Ash)
             print("=======================================================            ") 
             print("¡¡ Haz hecho un CRíTICO y Ash tiene ahora " + HP_Ash + " de vida !!") #feedback al usuario
             print("=======================================================            ")
             print()
-        elif Atq_esquivado > Atq_normal and Atq_esquivado > Atq_critico:
+        elif Atq_esquivado > Atq_normal and Atq_esquivado > Atq_critico: #Condición para que Ash esquive el ataque
             HP_Ash = HP_Ash * Esquivo
             str(HP_Ash)
             print("==============================================             ") 
             print("¡¡ Ash a ESQUIVADO y tiene ahora " + HP_Ash + " de vida !!    ") #feedback al usuario
             print("==============================================             ")
             print()
-        else:
+        else: #Condiciones sobrantes representan el ataque normal
             str(HP_Ash)
             print("=======================================            ") 
             print("¡¡Muy bien, Ash tiene ahora " + HP_Ash + "de vida!!") #feedback al usuario
             print("=======================================            ")
             print()
-    elif Atq_elegido == Atq_jugador[2]: #condición empleando como base el tercer término de la lista atq_jugador
-        HP_Ash = HP_Ash - random.randrange(22,48)
-        if Atq_critico > Atq_normal and Atq_critico > Atq_esquivado:
+    elif Atq_elegido == Atq_jugador[2]: #condición para ejecutar ataque Ascuas
+        HP_Ash = HP_Ash - random.randrange(22,49)
+        if Atq_critico > Atq_normal and Atq_critico > Atq_esquivado: #Condición para provocar un ataque crítico 
             HP_Ash = HP_Ash * Critico
             str(HP_Ash)
             print("=======================================================            ") 
             print("¡¡ Haz hecho un CRíTICO y Ash tiene ahora " + HP_Ash + " de vida !!") #feedback al usuario
             print("=======================================================            ")
             print()
-        elif Atq_esquivado > Atq_normal and Atq_esquivado > Atq_critico:
+        elif Atq_esquivado > Atq_normal and Atq_esquivado > Atq_critico: #Condición para que Ash esquive el ataque
             HP_Ash = HP_Ash * Esquivo
             str(HP_Ash)
             print("==============================================             ") 
             print("¡¡ Ash a ESQUIVADO y tiene ahora " + HP_Ash + " de vida !!    ") #feedback al usuario
             print("==============================================             ")
             print()
-        else:        
+        else: #Condiciones sobrantes representan el ataque normal        
             str(HP_Ash)
             print("========================================            ") 
             print("¡¡Sigue así, Ash tiene ahora " + HP_Ash + "de vida!!") #feedback al usuario
             print("========================================            ")
             print()
-
-    #Turno de Ash
-
-
-
