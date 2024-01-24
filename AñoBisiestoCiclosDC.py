f046e82ccd862c622d6dd72ce29de57e4057494c
DanielC218 <danielcarrer2007@gmail.com>
2024-01-23 20:23:18 -0400

    Actualización años bisiestros

--- AñoBisiestoCiclosDC.py
+++ AñoBisiestoCiclosDC.py
@@ -2,20 +2,20 @@ inicio = int(input("Porfavor escriba un número entero entre 1900 y 2199: "))
 año = inicio + 1
 año_usuario = inicio
 año_inicio = 1900
-biciesto = 0
-no_biciesto = 0
+bisiesto = 0
+no_bisiesto = 0
 
 while año > 1899:
     año = año - 1
     if año == año_inicio: 
-        print ("Enetre 1900 y " + str(año_usuario) + " hay " + str(biciesto) + " año(s) biciestro(s) y hay " + str(no_biciesto) + " año(s) no biciestro(s).")
+        print ("Enetre 1900 y " + str(año_usuario) + " hay " + str(bisiesto) + " año(s) biciestro(s) y hay " + str(no_bisiesto) + " año(s) no biciestro(s).")
         quit()
     else:
         if año % 4 != 0:
-            no_biciesto = no_biciesto + 1
+            no_bisiesto = no_bisiesto + 1
         elif año % 4 == 0 and año % 100 != 0 and año % 400 != 0:
-            biciesto = biciesto + 1
+            bisiesto = bisiesto + 1
         elif año % 4 == 0 and año % 100 == 0 and año % 400 != 0:
-            no_biciesto = no_biciesto + 1
+            no_bisiesto = no_bisiesto + 1
         elif año % 4 == 0 and año % 100 == 0 and año % 400 == 0:
-            biciesto = biciesto + 1
+            bisiesto = bisiesto + 1
--- AñoBisiestoDC.py
+++ AñoBisiestoDC.py
@@ -1,10 +1,14 @@
 inicio = int(input("Bienvenido a la calculadora de años bisiestos, escriba un número entero entre 1900 y 2199: "))
-año = inicio + 1
+año = inicio
 año_usuario = inicio
-año_inicio = 1900
-biciesto = 0
+bisiesto = año_usuario - 1900
+bisiesto = int(bisiesto / 4)
 
 if año % 4 == 0 and año % 100 != 0 and año % 400 != 0:
-    biciesto = biciesto + 1
+    bisiesto = bisiesto + 1
+elif año % 4 == 0 and año % 100 == 0 and año % 400 != 0:
+    bisiesto = bisiesto - 1
 elif año % 4 == 0 and año % 100 == 0 and año % 400 == 0:
-    biciesto = biciesto + 1
+    bisiesto = bisiesto + 1
+    
+print("Enetre 1900 y " + str(año_usuario) + " hay " + str(bisiesto) + " año(s) biciestro(s)")
